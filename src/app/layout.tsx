923e00cc74e9adb650d748483d40d6197beec8dd
Ashfaq Nisar <ashfaqnisar@celeritaz.com>
2024-06-08 17:34:10 +0530

    fix: Fixed the metadata issue.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { type ReactNode } from "react";
+import { type Metadata } from "next";
 
 import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
@@ -8,6 +9,16 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
 import TopBar from "./topbar";
+
+export const metadata: Metadata = {
+  title: "Ashfaq Nisar",
+  description: "Portfolio of Ashfaq Nisar",
+  openGraph: {
+    title: "Ashfaq Nisar",
+    description: "Portfolio of Ashfaq Nisar",
+    type: "website"
+  }
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
